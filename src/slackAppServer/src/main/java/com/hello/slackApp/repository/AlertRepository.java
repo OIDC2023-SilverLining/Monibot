6da7c11183c9f9963ad5f16df20f120c66be1a11
kyunghwan.yu <dbrudghks333@naver.com>
2023-08-06 15:07:13 +0900

    synchronize db

---
+++ src/slackAppServer/src/main/java/com/hello/slackApp/repository/AlertRepository.java
@@ -0,0 +1,56 @@
+// src/main/java/com/hello/slackApp/repository/AlertRepository.java
+
+package com.hello.slackApp.repository;
+
+import com.hello.slackApp.model.Alert;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class AlertRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AlertRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Alert> findAll() {
+        return jdbcTemplate.query(
+                "SELECT * FROM alerts",
+                (rs, rowNum) -> new Alert(rs.getString("type"),
+                        rs.getString("metric"),
+                        rs.getString("threshold"),
+                        rs.getString("condition"),
+                        rs.getString("duration")));
+    }
+
+    public void save(Alert alert) {
+        jdbcTemplate.update(
+                "INSERT INTO alerts(type, metric, threshold, condition, duration) VALUES (?, ?, ?, ?, ?)",
+                alert.getType(),
+                alert.getMetric(),
+                alert.getThreshold(),
+                alert.getCondition(),
+                alert.getDuration());
+    }
+    
+    public Alert findByMetric(String metric) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM alerts WHERE metric = ?",
+                new Object[]{metric},
+                (rs, rowNum) -> new Alert(rs.getString("type"),
+                        rs.getString("metric"),
+                        rs.getString("threshold"),
+                        rs.getString("condition"),
+                        rs.getString("duration")));
+    }    
+
+    public void delete(String metric) {
+        jdbcTemplate.update(
+                "DELETE FROM alerts WHERE metric = ?",
+                metric);
+    }
+    
+}
